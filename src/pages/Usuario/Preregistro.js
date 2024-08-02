3eef83372758b2921ed7a76a7507c85e48efd0c3
Daniel <acosta.daniel.grupo12@gmail.com>
2024-08-02 14:50:41 -0600

    cambios v4.1

--- src/pages/Usuario/Preregistro.js
+++ src/pages/Usuario/Preregistro.js
@@ -412,7 +412,7 @@ function Preregistro() {
     // Función para verificar si todas las casillas de verificación están marcadas
     const todasSeleccionadas = () => {
       // Verificar el estado de todas las variables de estado y devolver true si todas están marcadas
-      return video != null && ArchivoINE != null && ArchivoComprobanteDomicilio != null && ArchivoCURP != null && ArchivoRFC != null && ArchivoAval != null && tipoEntidad != null && entidad != null  && direccion != null && municipio_direccion != null && estado != null && cp != null && puesto != null && area != null && telefono != null && correo != null && confirma_correo != null && contrasena != null && confirma_contrasena != null  && isResponsavilidadUso != null && isPoliticas != null /* Agregar el resto de tus variables de estado */;
+      return video != null && ArchivoINE != null && ArchivoComprobanteDomicilio != null && ArchivoCURP != null && ArchivoRFC != null && ArchivoAval != null && tipoEntidad != null && entidad != null  && direccion != null && municipio_direccion != null && estado != null && cp != null && puesto != null && area != null && telefono != null && correo != null && confirma_correo != null &&  isResponsavilidadUso != null && isPoliticas != null /* Agregar el resto de tus variables de estado */;
     
   };
   
@@ -1203,82 +1203,6 @@ function Preregistro() {
           placeholder="Confirme su correo"
           className={!correosCoinciden ? 'rojo' : ''} // Agrega una clase rojo si los correos no coinciden
         />
-        </div>
-
-        <div className='text_formulario is-required'>
-        <span style={{ fontWeight: 'bold' }}>Contraseña/ Confirme su Contraseña  </span> 
-        </div>
-        <div style={{ display: 'inline-block', width: '85%', fontWeight: 'bold' }}>
-          <span className='text_formulario is-required' style={{ float: 'left' }}>Contraseña</span>        
-          <span className='text_formulario is-required' style={{ float: 'right' }}>Confirme su Contraseña</span>
-        </div> 
-
-        <div className="custom-inputs">
-          <div className="input-container">
-            <input
-              type={mostrarContrasena ? "text" : "password"}
-              value={contrasena}
-              onChange={(e) => setContrasena(e.target.value)}
-              placeholder="Contraseña"
-              minLength={8}
-              maxLength={15}
-              className="custom-input"
-            />
-            <label htmlFor="password"></label>
-            <div className="eye-icon" onClick={() => setMostrarContrasena(!mostrarContrasena)}>
-              {mostrarContrasena ? (
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" height={"1.5rem"}>
-                  <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
-                  <path fillRule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clipRule="evenodd" />
-                </svg>
-              ) : (
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" height={"1.5rem"}>
-                  <path d="M3.53 2.47a.75.75 0 00-1.06 1.06l18 18a.75.75 0 101.06-1.06l-18-18zM22.676 12.553a11.249 11.249 0 01-2.631 4.31l-3.099-3.099a5.25 5.25 0 00-6.71-6.71L7.759 4.577a11.217 11.217 0 014.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113z" />
-                  <path d="M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0115.75 12zM12.53 15.713l-4.243-4.244a3.75 3.75 0 004.243 4.243z" />
-                  <path d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 00-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 016.75 12z" />
-                </svg>
-              )}
-            </div>
-          </div>
-          <div className="input-container">
-            <input
-              type={mostrarConfirmaContrasena ? 'text' : 'password'}
-              value={confirma_contrasena}
-              onChange={(e) => {
-                const value = e.target.value;
-                setConfirma_Contrasena(value);
-                setContrasenasCoinciden(value === contrasena);
-              }}
-              placeholder="Confirma tu contraseña"
-              minLength={8}
-              maxLength={15}
-              className={`custom-input ${!contrasenasCoinciden ? 'rojo' : ''}`}
-            />
-            <label htmlFor="password"></label>
-            <div className="eye-icon" onClick={() => setMostrarConfirmaContrasena(!mostrarConfirmaContrasena)}>
-              {mostrarConfirmaContrasena ? (
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" height={"1.5rem"}>
-                  <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
-                  <path fillRule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clipRule="evenodd" />
-                </svg>
-              ) : (
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" height={"1.5rem"}>
-                  <path d="M3.53 2.47a.75.75 0 00-1.06 1.06l18 18a.75.75 0 101.06-1.06l-18-18zM22.676 12.553a11.249 11.249 0 01-2.631 4.31l-3.099-3.099a5.25 5.25 0 00-6.71-6.71L7.759 4.577a11.217 11.217 0 014.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113z" />
-                  <path d="M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0115.75 12zM12.53 15.713l-4.243-4.244a3.75 3.75 0 004.243 4.243z" />
-                  <path d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 00-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 016.75 12z" />
-                </svg>
-              )}
-            </div>
-          </div>
-        </div>
-
-
-        <div className='text_advertencia'>
-        <span>La contraseña deberá contener un mínimo de 8 y un máximo de 15 caracteres.</span> 
-        </div>
-
-        <div className='text_advertencia2'>
-        <span>Utilizar letras y números, no debe usar caracteres especiales.</span> 
         </div>
 
         <div style={{  marginTop: '2%' }} className="checkboxes">
