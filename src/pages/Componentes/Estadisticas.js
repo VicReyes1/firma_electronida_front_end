086d94ffc807d47953e885de7e6187fb2a2ec8b9
Daniel <acosta.daniel.grupo12@gmail.com>
2024-10-14 16:06:18 -0600

    filtro de estadisticas

--- src/pages/Componentes/Estadisticas.js
+++ src/pages/Componentes/Estadisticas.js
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
+import DatePicker, { registerLocale } from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css"; // Importa los estilos del DatePicker
+import { setMonth, setYear } from 'date-fns'; 
+import { es } from 'date-fns/locale';
+registerLocale('es', es);
 
 const Estadisticas = () => {
+  registerLocale('es', es);
+
   const [data, setData] = useState({ nuevo: 0, enCurso: 0, concluida: 0, suspendida: 0 });
   const apiUrl = process.env.REACT_APP_API_URL;
   const token = localStorage.getItem('token');
@@ -67,11 +74,26 @@ const Estadisticas = () => {
     }]
   };
 
+  const [startDate, setStartDate] = useState(new Date());
+  const handleDateChange = (date) => {
+    setStartDate(date);
+    //data.filter()
+  };
+
   useEffect(() => {
-    fetch(`${apiUrl}/admin/estadisticas`,{
+    const selectedMonth = startDate.getMonth() + 1; // getMonth() es 0-indexado
+    const selectedYear = startDate.getFullYear();
+
+    fetch(`${apiUrl}/admin/estadisticas`, {
+      method: 'POST', // Cambiar a POST
       headers: {
-        'Authorization': `${token}`
-      }
+        'Authorization': `${token}`,
+        'Content-Type': 'application/json' // Asegúrate de establecer el tipo de contenido
+      },
+      body: JSON.stringify({
+        month: selectedMonth, // Enviar el mes
+        year: selectedYear // Enviar el año
+      })
     })
       .then(response => {
         if (!response.ok) {
@@ -80,16 +102,27 @@ const Estadisticas = () => {
         return response.json();
       })
       .then(data => {
-        setData(data);
+        setData(data); // Actualizar el estado con los nuevos datos
       })
       .catch(error => {
         console.error('Error fetching data:', error);
       });
-  }, []);
+  }, [startDate]);
 
   return (
     <div>
       <h1>Gráficos de Estado de Solicitudes</h1>
+
+      <DatePicker
+        selected={startDate}
+        onChange={handleDateChange}
+        dateFormat="MM/yyyy"
+        showMonthYearPicker // Solo muestra mes y año
+        showFullMonthYearPicker // Muestra el selector completo de mes y año
+        showTwoColumnMonthYearPicker // Opcional: dos columnas para una mejor vista
+        locale="es"
+      />
+
       <HighchartsReact
         highcharts={Highcharts}
         options={columnOptions}
